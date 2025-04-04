edee77c7c8ef5e87e7b4674cd343c6f21f3b31f6
Arian <ariyanfar81@gmail.com>
2025-04-04 14:16:18 +0330

    RPS

--- python/RPS_2025.py
+++ python/RPS_2025.py
@@ -7,9 +7,31 @@ class RPS:
         print("welcome to RPS game!")
         self.choices = {'rock':'🪨','paper':'📃','scissors':'✂️'}
         self.valid_choices = list(self.choices.keys())
+        self.play_game()
         
     def play_game(self):
+        user_choice : str = input("Rock,Paper or Scissors >>>> ").lower()
         
-    def display(self):
+        if user_choice == "exit":
+            print("Hope to see you again,bye👋🏼")
+            sys.exit()
+        elif user_choice is not self.valid_choices:
+            print(f"please choose among valid choices >>> {self.valid_choices}")
+            return self.play_game()
+        else:
+            ai_choice = choice([self.valid_choices])
+            
+            self.display(user_choice,ai_choice)
         
-    def check(self):
+                
+             
+        
+    def display(self,user_choice,ai_choice):
+        ...
+        
+    def check(self,user_choice,ai_choice):
+        ...    
+
+if __name__ == "__main__":
+    game = RPS()
+    game.play_game()    
