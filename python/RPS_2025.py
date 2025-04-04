9e75ad1b7d6443d7d3fe9c1a51ad5a4ffe367a18
Arian <ariyanfar81@gmail.com>
2025-04-04 13:53:55 +0330

    RPS

---
+++ python/RPS_2025.py
@@ -0,0 +1,15 @@
+from random import choice
+import sys
+
+
+class RPS:
+    def __init__(self):
+        print("welcome to RPS game!")
+        self.choices = {'rock':'🪨','paper':'📃','scissors':'✂️'}
+        self.valid_choices = list(self.choices.keys())
+        
+    def play_game(self):
+        
+    def display(self):
+        
+    def check(self):
