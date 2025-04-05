c5bbd9b1451b35e5e5af99f19112140dfea4c4e4
Arian <ariyanfar81@gmail.com>
2025-04-05 13:22:05 +0330

    password generator

--- python/Password_Generator.py
+++ python/Password_Generator.py
@@ -2,3 +2,17 @@ import string
 import secrets
 
 
+def upper_check(password) -> bool:
+    for char in password:
+        if char.isupper():
+            return True
+    return False
+
+def symbol_check(password) -> bool:
+    for char in password:
+        if char in string.punctuation:
+            return True
+    return False
+
+def password_generator(length:int,upper_check: bool,symbol_check:bool):
+    
