4efd0708a0dc90a33a9046b524efe3068047a213
Arian <ariyanfar81@gmail.com>
2025-04-05 13:41:43 +0330

    Password Generator

--- python/Password_Generator.py
+++ python/Password_Generator.py
@@ -1,40 +1,42 @@
 import string
 import secrets
 
+class PasswordGenerator:
+    def __init__(self):
+        self.combination = string.ascii_lowercase + string.digits
 
-def upper_check(password) -> bool:
-    for char in password:
-        if char.isupper():
-            return True
-    return False
+    def upper_check(self, password) -> bool:
+        for char in password:
+            if char.isupper():
+                return True
+        return False
 
-def symbol_check(password) -> bool:
-    for char in password:
-        if char in string.punctuation:
-            return True
-    return False
+    def symbol_check(self, password) -> bool:
+        for char in password:
+            if char in string.punctuation:
+                return True
+        return False
 
-def password_generator(length:int,upper_check: bool,symbol_check:bool):
-    
-    combination = string.ascii_lowercase + string.digits
-    
-    if upper_check:
-        combination += string.ascii_uppercase
-    
-    if symbol_check:
-        combination += string.punctuation
-        
-    combination_length = len(combination)
-    new_password = ''
-    
-    for i in range(length):
-        # Use secrets.choice for cryptographic security
-        new_password += combination[secrets.randbelow(combination_length)] 
+    def generate_password(self, length: int, upper_check: bool, symbol_check: bool) -> str:
+        combination = self.combination
+        if upper_check:
+            combination += string.ascii_uppercase
+        if symbol_check:
+            combination += string.punctuation
 
-    return new_password
-    
+        combination_length = len(combination)
+        new_password = ''
+        for i in range(length):
+            # Use secrets.choice for cryptographic security
+            new_password += combination[secrets.randbelow(combination_length)]
+        return new_password
+
+    def run(self):
+        for i in range(5):
+            new_password = self.generate_password(length=15, symbol_check=True, upper_check=True)
+            check = f'U:{self.upper_check(new_password)}, S:{self.symbol_check(new_password)}'
+            print(f'P{i+1} -----> {new_password} | ({check})')
+
 if __name__ == "__main__":
-    for i in range(5):
-        new_password = password_generator(length=15,symbol_check=False,upper_check=False)
-        check = f'U:{upper_check(new_password)}, S:{symbol_check(new_password)}'
-        print(f'P{i+1} -----> {new_password} | ({check})')
+    generator = PasswordGenerator()
+    generator.run()
