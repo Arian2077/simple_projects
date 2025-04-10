39bb2447d894205da59932ec594f8f0d4fc6a359
Arian <ariyanfar81@gmail.com>
2025-04-10 22:22:29 +0330

    commit

--- python/Website_checker/Website_checker.py
+++ python/Website_checker/Website_checker.py
@@ -8,5 +8,14 @@ def get_websites(csv_path: str) -> list[str]:
     with open(csv_path, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            if 'https://' in row[1]:
-                websites.append(f'http://{row[0]}')
+            if 'https://' not in row[1]:
+                websites.append(f'https://{row[1]}')
+            else:
+                websites.append(row[1])
+            
+        return websites
+    
+def get_user_agent() -> str:
+    user_agent = UserAgent()
+    return user_agent.chrome
+print(get_user_agent())
