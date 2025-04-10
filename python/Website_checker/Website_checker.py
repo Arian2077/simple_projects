4a5e235b0afa13b1dcd6357d88acf600dfbdf3d3
Arian <ariyanfar81@gmail.com>
2025-04-10 22:38:08 +0330

    Website_Checker

--- python/Website_checker/Website_checker.py
+++ python/Website_checker/Website_checker.py
@@ -1,36 +1,56 @@
 import csv
 import requests
-from fake_useragent import UserAgent
 from http import HTTPStatus
+from fake_useragent import UserAgent
 
-def get_websites(csv_path: str) -> list[str]:
-    websites: list[str] = []
-    with open(csv_path, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if 'https://' not in row[1]:
-                websites.append(f'https://{row[1]}')
-            else:
-                websites.append(row[1])
-            
+
+class WebsiteChecker:
+    def __init__(self, csv_path: str):
+        self.csv_path = csv_path
+        self.user_agent = self.get_user_agent()
+
+    def get_websites(self) -> list[str]:
+        """Loads websites from a csv file"""
+        websites: list[str] = []
+        with open(self.csv_path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if 'https://' not in row[1] or 'http://' not in row[1]:
+                    websites.append(f'https://{row[1]}')
+                else:
+                    websites.append(row[1])
         return websites
-    
-def get_user_agent() -> str:
-    user_agent = UserAgent()
-    return user_agent.chrome
+
+    @staticmethod
+    def get_user_agent() -> str:
+        """Returns a user agent that can be used with requests"""
+        ua = UserAgent()
+        return ua.chrome
+
+    @staticmethod
+    def get_status_description(status_code: int) -> str:
+        """Uses the status code to return a readable description"""
+        for value in HTTPStatus:
+            if value == status_code:
+                description: str = f'({value} {value.name}) {value.description}'
+                return description
+        return '(???) Unknown status code...'
+
+    def check_website(self, website: str):
+        """Gets the status code for a website and prints the result"""
+        try:
+            code: int = requests.get(website, headers={'User-Agent': self.user_agent}).status_code
+            print(website, self.get_status_description(code))
+        except Exception:
+            print(f'**Could not get information for website: "{website}"')
+
+    def run(self):
+        """Main method to check all websites"""
+        sites: list[str] = self.get_websites()
+        for site in sites:
+            self.check_website(site)
 
 
-def get_status_description(url: int) -> str:
-    for value in HTTPStatus:
-        if value == url:
-            description: str = f'({value} {value.name}) {value.description}'
-            return description
-        
-    return '(???) Unknown status code...'
-
-def check_website_status(websites:str , user_agent):
-    try:
-        code:int = requests.get(websites, headers={'User-Agent': user_agent}).status_code
-        print(websites,get_status_description(code)) 
-    except Exception:
-        print(websites, 'Error: Unable to connect to the website.')
+if __name__ == '__main__':
+    checker = WebsiteChecker('Website_checker/websites.csv')
+    checker.run()
