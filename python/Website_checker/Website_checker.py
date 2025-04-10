b9d16b522373b3ddd016d2f9c4b440e0e3e96264
Arian <ariyanfar81@gmail.com>
2025-04-10 22:07:51 +0330

    commit

--- python/Website_checker/Website_checker.py
+++ python/Website_checker/Website_checker.py
@@ -4,4 +4,9 @@ from fake_useragent import UserAgent
 from http import HTTPStatus
 
 def get_websites(csv_path: str) -> list[str]:
-    pass
+    websites: list[str] = []
+    with open(csv_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if 'https://' in row[1]:
+                websites.append(f'http://{row[0]}')
