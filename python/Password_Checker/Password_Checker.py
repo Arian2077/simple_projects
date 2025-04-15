cf64580122712fd990a77df5e259144f48680da5
Arian <ariyanfar81@gmail.com>
2025-04-15 23:59:08 +0330

    commit

--- python/Password_Checker/Password_Checker.py
+++ python/Password_Checker/Password_Checker.py
@@ -3,5 +3,9 @@ import string
 import time
 
 def common_guess(word: str) -> str | None:
-    with open("words.txt", "r") as file:
-        words = file.read().splitlines()
+    with open("words.txt", "r") as words:
+        word_list : list[str] = words.read().splitlines()   
+        
+    for i, match in enumerate(word_list,start=1):
+        if match == word:
+            return f'Common match: {match} found at position {i}'
